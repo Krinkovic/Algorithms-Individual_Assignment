e0e6e0f988d6295c0a807f998655843c63ca5c10
Krinkovic <186858802+Krinkovic@users.noreply.github.com>
2026-05-14 17:50:23 +0200

    Updated function signatures and wrote the implementation for the brute force algorithm

--- include/string-search.h
+++ include/string-search.h
@@ -3,6 +3,8 @@
 #ifndef BOYER_MOORE_H
 #define BOYER_MOORE_H
 
+#include <stdlib.h>
+
 //Implements brute-force string matching
 //Input: An array T [0..n − 1] of n characters representing a text and
 //
@@ -10,8 +12,8 @@
 //Output: The index of the first character in the text that starts a
 //
 // matching substring or −1 if the search is unsuccessful
-int  bruteForce(char*, char*);
+int bruteForce(char* text, size_t tlength, char* pattern, size_t plength);
 
-int boyerMoore(char*, char*);
+int boyerMoore(char* text, size_t tlength, char* pattern, size_t plength);
 
 #endif
--- src/string-search.c
+++ src/string-search.c
@@ -2,29 +2,36 @@
 //
 // Implementation of the Boyer-Moore algorithm
 
+#include <stdlib.h>
 #include "../include/string-search.h"
 
 //Implements brute-force string matching
-//Input: An array T [0..n − 1] of n characters representing a text and
+//Input: An array T[0..n − 1] of n characters representing a text and
 //
-// an array P [0..m − 1] of m characters representing a pattern
+// an array P[0..m − 1] of m characters representing a pattern
 //Output: The index of the first character in the text that starts a
 //
 // matching substring or −1 if the search is unsuccessful
 //
 // for i <- 0 to n − m do
     // j <- 0
-    // while j < m and P [j ] = T [i + j ] do
-        // j <- j +1
+    // while j < m and P[j] = T[i + j] do
+        // j <- j + 1
     // if j = m return i
 // return −1
-int bruteForce(char* text, char* pattern)
+int bruteForce(char* text, size_t tlength, char* pattern, size_t plength)
 {
-    int i;
-    return i;
+    for (int i = 0; i <= tlength - plength; i++) {
+        int j = 0;
+        while (j < plength && pattern[j] == text[i + j]) {
+            j = j + 1;
+        }
+        if (j == plength) return i;
+    }
+    return -1;
 }
 
-int boyerMoore(char* text, char* pattern)
+int boyerMoore(char* text, size_t tlength, char* pattern, size_t plength)
 {
     int i;
     return i;
